ce42e0b45444ebbcd02e9c740c431e95e3af6d8c
navas <gabrielnavas@email.com>
2023-07-02 19:41:23 -0300

    load product fake data json

--- product/product.go
+++ product/product.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func loadData() []byte {
+	jsonFile, err := os.Open("./products.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
+	data, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return data
+}
+
 func main() {
-	println("oi")
+	log.Println(string(loadData()))
 }
